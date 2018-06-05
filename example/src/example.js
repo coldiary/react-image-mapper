5d059499d86a893d07f2c5313d6e488ee1dfd13d
Benoit Sida <benoit.sida@gmail.com>
2018-06-05 23:46:02 +0200

    update example adding event handlers

--- example/src/example.js
+++ example/src/example.js
@@ -15,11 +15,45 @@ var MAP = {
 var URL = 'https://c1.staticflickr.com/5/4052/4503898393_303cfbc9fd_b.jpg';
 
 var App = React.createClass({
+	getInitialState() {
+		return { msg: null };
+	},
+	load() {
+		this.setState({ msg: 'Interact with image !' });
+	},
+	clicked(area) {
+		this.setState({ msg: `You clicked on ${area.shape} at coords ${JSON.stringify(area.coords)} !` });
+	},
+	clickedOutside(evt) {
+		const coords = {x: evt.nativeEvent.layerX, y: evt.nativeEvent.layerY };
+		this.setState({ msg: `You clicked on the image at coords ${JSON.stringify(coords)} !` });
+	},
+	enterArea(area) {
+		this.setState({ msg: `You entered ${area.shape} at coords ${JSON.stringify(area.coords)} !` });
+	},
+	leaveArea(area) {
+		this.setState({ msg: `You leaved ${area.shape} at coords ${JSON.stringify(area.coords)} !` });
+	},
 	render () {
 		return (
 			<div>
-				<ImageMapper src={URL} map={MAP} width={500}/>
-				<pre><code>{'<ImageMapper src={URL} map={MAP} width={500}/>'}</code></pre>
+				<ImageMapper src={URL} map={MAP} width={500}
+					onLoad={() => this.load()}
+					onClick={area => this.clicked(area)}
+					onMouseEnter={area => this.enterArea(area)}
+					onMouseLeave={area => this.leaveArea(area)}
+					onImageClick={evt => this.clickedOutside(evt)}
+				/>
+				<pre>{this.state.msg ? this.state.msg : null}</pre>
+				<pre><code>{`
+<ImageMapper src={URL} map={MAP} width={500}
+	onLoad={() => this.load()}
+	onClick={area => this.clicked(area)}
+	onMouseEnter={area => this.enterArea(area)}
+	onMouseLeave={area => this.leaveArea(area)}
+	onImageClick={evt => this.clickedOutside(evt)}
+/>
+				`}</code></pre>
 				<pre><code className="json">{
 					'URL = "https://c1.staticflickr.com/5/4052/4503898393_303cfbc9fd_b.jpg"\n' +
 					'MAP = {\n' +
