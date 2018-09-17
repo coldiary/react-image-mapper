cda7ee134e4c513870ae483385558f084815777a
Benoit Sida <benoit.sida@gmail.com>
2018-09-17 15:46:45 -0400

    Update example

--- example/src/example.js
+++ example/src/example.js
@@ -5,10 +5,10 @@ var ImageMapper = require('react-image-mapper');
 var MAP = {
 	name: 'my-map',
 	areas: [
-		{shape: 'poly', coords: [25,33,27,300,128,240,128,94]},
-		{shape: 'poly', coords: [219,118,220,210,283,210,284,119]},
-		{shape: 'poly', coords: [381,241,383,94,462,53,457,282]},
-		{shape: 'poly', coords: [245,285,290,285,274,239,249,238]},
+		{name: '1', shape: 'poly', coords: [25,33,27,300,128,240,128,94]},
+		{name: '2', shape: 'poly', coords: [219,118,220,210,283,210,284,119]},
+		{name: '3', shape: 'poly', coords: [381,241,383,94,462,53,457,282]},
+		{name: '4', shape: 'poly', coords: [245,285,290,285,274,239,249,238]},
 	]
 };
 
@@ -16,7 +16,7 @@ var URL = 'https://c1.staticflickr.com/5/4052/4503898393_303cfbc9fd_b.jpg';
 
 var App = React.createClass({
 	getInitialState() {
-		return { msg: null };
+		return { hoveredArea: null, msg: null };
 	},
 	load() {
 		this.setState({ msg: 'Interact with image !' });
@@ -29,41 +29,104 @@ var App = React.createClass({
 		this.setState({ msg: `You clicked on the image at coords ${JSON.stringify(coords)} !` });
 	},
 	enterArea(area) {
-		this.setState({ msg: `You entered ${area.shape} at coords ${JSON.stringify(area.coords)} !` });
+		this.setState({ hoveredArea: area, msg: `You entered ${area.shape} at coords ${JSON.stringify(area.coords)} !` });
 	},
 	leaveArea(area) {
-		this.setState({ msg: `You leaved ${area.shape} at coords ${JSON.stringify(area.coords)} !` });
+		this.setState({ hoveredArea: null, msg: `You leaved ${area.shape} at coords ${JSON.stringify(area.coords)} !` });
 	},
+
+	getTipPosition(area) {
+		if (!area) return { top: 0, left: 0 };
+		// Calculate centroid
+		const n = area.coords.length / 2;
+		const { y, x } = area.coords.reduce(({ y, x }, val, idx) => {
+			return !(idx % 2) ? { y, x: x + (val / n) } : { y: y + (val / n), x };
+		}, { y: 0, x: 0 });
+		return { top: `${y}px`, left: `${x}px` };
+	},
+
 	render () {
 		return (
 			<div>
-				<ImageMapper src={URL} map={MAP} width={500}
-					onLoad={() => this.load()}
-					onClick={area => this.clicked(area)}
-					onMouseEnter={area => this.enterArea(area)}
-					onMouseLeave={area => this.leaveArea(area)}
-					onImageClick={evt => this.clickedOutside(evt)}
-				/>
+				<div style={{ position: 'relative' }}>
+					<ImageMapper src={URL} map={MAP} width={500}
+						onLoad={() => this.load()}
+						onClick={area => this.clicked(area)}
+						onMouseEnter={area => this.enterArea(area)}
+						onMouseLeave={area => this.leaveArea(area)}
+						onImageClick={evt => this.clickedOutside(evt)}
+					/>
+					{
+						this.state.hoveredArea &&
+						<span className="tooltip" style={{ ...this.getTipPosition(this.state.hoveredArea)}}>
+							{ this.state.hoveredArea && this.state.hoveredArea.name}
+						</span>
+					}
+				</div>
 				<pre>{this.state.msg ? this.state.msg : null}</pre>
-				<pre><code>{`
-<ImageMapper src={URL} map={MAP} width={500}
-	onLoad={() => this.load()}
-	onClick={area => this.clicked(area)}
-	onMouseEnter={area => this.enterArea(area)}
-	onMouseLeave={area => this.leaveArea(area)}
-	onImageClick={evt => this.clickedOutside(evt)}
-/>
-				`}</code></pre>
+				<pre><code>{
+					'<ImageMapper src={URL} map={MAP} width={500}\n' +
+					'    onLoad={() => this.load()}\n' +
+					'    onClick={area => this.clicked(area)}\n' +
+					'    onMouseEnter={area => this.enterArea(area)}\n' +
+					'    onMouseLeave={area => this.leaveArea(area)}\n' +
+					'    onImageClick={evt => this.clickedOutside(evt)}\n' +
+					'/>\n'
+				}</code></pre>
 				<pre><code className="json">{
 					'URL = "https://c1.staticflickr.com/5/4052/4503898393_303cfbc9fd_b.jpg"\n' +
 					'MAP = {\n' +
 					'  name: "my-map",\n' +
 					'  areas: [\n' +
-					'    { shape: "poly", coords: [25,33,27,300,128,240,128,94] },\n' +
-					'    { shape: "poly", coords: [219,118,220,210,283,210,284,119] },\n' +
-					'    { shape: "poly", coords: [381,241,383,94,462,53,457,282] },\n' +
-					'    { shape: "poly", coords: [245,285,290,285,274,239,249,238] },\n' +
+					'    { name: "1", shape: "poly", coords: [25,33,27,300,128,240,128,94] },\n' +
+					'    { name: "2", shape: "poly", coords: [219,118,220,210,283,210,284,119] },\n' +
+					'    { name: "3", shape: "poly", coords: [381,241,383,94,462,53,457,282] },\n' +
+					'    { name: "4", shape: "poly", coords: [245,285,290,285,274,239,249,238] },\n' +
 					'  ]\n}'
+				}</code></pre>
+				Example with custom tooltips:
+				<pre><code>{
+					'<div className="container">\n' +
+					'    <ImageMapper src={URL} map={MAP} width={500}\n' +
+					'    	onLoad={() => this.load()}\n' +
+					'    	onClick={area => this.clicked(area)}\n' +
+					'    	onMouseEnter={area => this.enterArea(area)}\n' +
+					'    	onMouseLeave={area => this.leaveArea(area)}\n' +
+					'    	onImageClick={evt => this.clickedOutside(evt)}\n' +
+					'    />\n' +
+					'    {\n' +
+					'    	this.state.hoveredArea &&\n' +
+					'    	<span className="tooltip" style={{ ...this.getTipPosition(this.state.hoveredArea)}}>\n' +
+					'    		{ this.state.hoveredArea && this.state.hoveredArea.name}\n' +
+					'    	</span>\n' +
+					'    }\n' +
+					'</div>\n'
+				}</code></pre>
+				<pre><code className="js">{
+					'getTipPosition(area) {\n' +
+					'    if (!area) return { top: 0, left: 0 };\n' +
+					'    // Calculate centroid\n' +
+					'    const n = area.coords.length / 2;\n' +
+					'    const { y, x } = area.coords.reduce(({ y, x }, val, idx) => {\n' +
+					'    	return !(idx % 2) ? { y, x: x + (val / n) } : { y: y + (val / n), x };\n' +
+					'    }, { y: 0, x: 0 });\n' +
+					'    return { top: `${y}px`, left: `${x}px` };\n' +
+					'},\n'
+				}</code></pre>
+				<pre><code className="css">{
+					'.container {\n' +
+					'    position: relative;\n' +
+					'}\n\n' +
+					'.tooltip {\n' +
+					'    position: absolute;\n' +
+					'    color: #fff;\n' +
+					'    padding: 10px;\n' +
+					'    background: rgba(0,0,0,0.8);\n' +
+					'    transform: translate3d(-50%, -50%, 0);\n' +
+					'    border-radius: 5px;\n' +
+					'    pointer-events: none;\n' +
+					'    zIndex: 1000;\n' +
+					'}\n'
 				}</code></pre>
 			</div>
 		);
