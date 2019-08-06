a4dcf8da09432194feef704ef90198355b4097d1
Sandesh Kumar <sandeshkumar@folio3.com>
2019-08-06 11:51:35 +0500

    updated example code

--- example/src/example.js
+++ example/src/example.js
@@ -10,7 +10,7 @@ var MAP = {
 			shape: "poly",
 			coords: [25, 33, 27, 300, 128, 240, 128, 94],
 			preFillColor: "green",
-			fillColor: "#0000ff",
+			fillColor: "#0000ff"
 		},
 		{
 			name: "2",
@@ -18,7 +18,7 @@ var MAP = {
 			coords: [219, 118, 220, 210, 283, 210, 284, 119],
 			preFillColor: "pink",
 			lineWidth: 10,
-			strokeColor: "#0000ff",
+			strokeColor: "#0000ff"
 		},
 		{
 			name: "3",
@@ -26,7 +26,7 @@ var MAP = {
 			coords: [381, 241, 383, 94, 462, 53, 457, 282],
 			preFillColor: "yellow", // this is mandatory for stroke color to work
 			lineWidth: 10,
-			strokeColor: "#6afd09",
+			strokeColor: "#6afd09"
 		},
 		{
 			name: "4",
@@ -39,15 +39,15 @@ var MAP = {
 			shape: "circle",
 			coords: [170, 100, 25],
 			preFillColor: "rgb(255,255,255,0.3)",
-			lineWidth: 2,
+			lineWidth: 2
 		},
 		{
 			name: "6",
 			shape: "rect",
 			coords: [270, 100, 200, 50],
 			lineWidth: 2,
-            preFillColor: "rgba(255, 255, 255, 0.3)",
-			strokeColor: "#6afd09",
+			preFillColor: "rgba(255, 255, 255, 0.3)",
+			strokeColor: "#6afd09"
 		}
 	]
 };
@@ -55,7 +55,6 @@ var MAP = {
 var URL = "https://c1.staticflickr.com/5/4052/4503898393_303cfbc9fd_b.jpg";
 
 var App = React.createClass({
-
 	getInitialState() {
 		return { hoveredArea: null, msg: null, moveMsg: null };
 	},
@@ -172,11 +171,12 @@ var App = React.createClass({
 								"MAP = {\n" +
 								'  name: "my-map",\n' +
 								"  areas: [\n" +
-								'    { name: "1", shape: "poly", coords: [25,33,27,300,128,240,128,94], preFillColor: "green", fillColor: "blue"  },\n' +
-								'    { name: "2", shape: "poly", coords: [219,118,220,210,283,210,284,119], preFillColor: "pink"  },\n' +
-								'    { name: "3", shape: "poly", coords: [381,241,383,94,462,53,457,282], fillColor: "yellow"  },\n' +
+								'    { name: "1", shape: "poly", coords: [25,33,27,300,128,240,128,94], preFillColor: "green", fillColor: "#0000ff", },\n' +
+								'    { name: "2", shape: "poly", coords: [219,118,220,210,283,210,284,119], preFillColor: "pink", lineWidth: 10, strokeColor: "#0000ff" },\n' +
+								'    { name: "3", shape: "poly", coords: [381,241,383,94,462,53,457,282], preFillColor: "yellow", /*this is mandatory for stroke color to work*/ lineWidth: 10, strokeColor: "#6afd09" },\n' +
 								'    { name: "4", shape: "poly", coords: [245,285,290,285,274,239,249,238], preFillColor: "red"  },\n' +
-								'    { name: "5", shape: "circle", coords: [170, 100, 25 ] },\n' +
+								'    { name: "5", shape: "circle", coords: [170, 100, 25 ], preFillColor: "rgb(255,255,255,0.3)", lineWidth: 2 },\n' +
+								'    { name: "6", shape: "rect", coords: [270, 100, 200, 50], lineWidth: 2, preFillColor: "rgba(255, 255, 255, 0.3)", strokeColor: "#6afd09" }\n' +
 								"  ]\n}"}
 						</code>
 					</pre>
